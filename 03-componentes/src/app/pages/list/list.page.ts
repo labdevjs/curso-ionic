f77e104fe9279053cfe8d60a9a9138b7b2271efa
labdevjs <labdevjs@gmail.com>
2021-04-20 11:52:47 +0000

    59

--- 03-componentes/src/app/pages/list/list.page.ts
+++ 03-componentes/src/app/pages/list/list.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { Observable } from 'rxjs';
+import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -8,6 +9,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./list.page.scss'],
 })
 export class ListPage implements OnInit {
+  @ViewChild('lista') lista: IonList;
 
   usuarios: Observable<any>;
 
@@ -15,6 +17,21 @@ export class ListPage implements OnInit {
 
   ngOnInit() {
     this.usuarios = this.dataService.getUsers();
+  }
+
+  favorite(user){
+    console.log('favorite', user);
+    this.lista.closeSlidingItems();
+  }
+
+  share(user){
+    console.log('share', user);
+    this.lista.closeSlidingItems();
+  }
+
+  borrar(user){
+    console.log('borrar', user);
+    this.lista.closeSlidingItems();
   }
 
 }
