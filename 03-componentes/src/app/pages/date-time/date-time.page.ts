5449c3da3ea1796cbba666595eb1e308b60a143b
labdevjs <labdevjs@gmail.com>
2021-04-20 07:47:44 +0000

    50

--- 03-componentes/src/app/pages/date-time/date-time.page.ts
+++ 03-componentes/src/app/pages/date-time/date-time.page.ts
@@ -8,12 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class DateTimePage implements OnInit {
 
   fechaNaci: Date = new Date();
+  customPickerOptions;
+  customDate;
+
   constructor() { }
 
   ngOnInit() {
+    this.customPickerOptions = {
+      buttons: [{
+        text: 'Save',
+        handler: (evento) => {
+          console.log('Clicked Save!');
+          console.log(evento);
+        }
+      }, {
+        text: 'Log',
+        handler: () => {
+          console.log('Clicked Log. Do not Dismiss.');
+          return false;
+        }
+      }]
+    }
   }
 
-  cambioFecha( event ) {
+  cambioFecha(event) {
     console.log('ionChange', event);
     console.log('Date', new Date(event.detail.value));
   }
