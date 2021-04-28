1e48795e60be5cd3313259cff465077338de7220
labdevjs <labdevjs@gmail.com>
2021-04-28 14:43:41 +0000

    106

--- 04-noticias/src/app/app.module.ts
+++ 04-noticias/src/app/app.module.ts
@@ -10,12 +10,13 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'
 
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, InAppBrowser],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, InAppBrowser, SocialSharing],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- 04-noticias/src/app/components/noticia/noticia.component.ts
+++ 04-noticias/src/app/components/noticia/noticia.component.ts
@@ -3,6 +3,7 @@ import { Article } from 'src/app/interfaces/interfaces';
 
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { ActionSheetController } from '@ionic/angular';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-noticia',
@@ -14,7 +15,8 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() indice: number;
   constructor(private iab: InAppBrowser,
-    private actionSheetCtrl: ActionSheetController) { }
+              private actionSheetCtrl: ActionSheetController,
+              private socialSharing: SocialSharing) { }
 
   ngOnInit() { }
 
@@ -31,6 +33,11 @@ export class NoticiaComponent implements OnInit {
         icon: 'share',
         cssClass: 'action-dark',
         handler: () => {
+          this.socialSharing.share(
+            this.noticia.title,
+            this.noticia.source.name,
+            '',
+            this.noticia.url)
           console.log('Share clicked');
         }
       },
