7c26f507fe4ba4d55ec3711aca2ba11a6a5f3afc
labdevjs <labdevjs@gmail.com>
2021-05-05 14:43:21 +0000

    142

--- 05-peliculasApp/src/app/tab2/tab2.page.ts
+++ 05-peliculasApp/src/app/tab2/tab2.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { MoviesService } from '../services/movies.service';
+import { Pelicula } from '../interfaces/interfaces';
+import { DetalleComponent } from '../components/detalle/detalle.component';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -9,22 +12,46 @@ import { MoviesService } from '../services/movies.service';
 export class Tab2Page {
 
   textoBuscar = '';
+  peliculas: Pelicula[] = [];
   ideas: string[] = ['Spiderman', 'Avenger', 'El señor de los anillos', 'La vida es bella'];
+  buscando = false;
 
-  constructor(private movieService: MoviesService) { }
+  constructor(private movieService: MoviesService,
+              private modalCtrl: ModalController) { }
 
   buscar(event) {
-    const valor = event.detail.value;
-    console.log(valor);
+    const valor: string = event.detail.value;
+    //console.log(valor);
+
+    if (valor.length === 0) {
+      this.peliculas = [];
+      return;
+    }
+
+    this.buscando = true;
     this.movieService.buscarPelicula(valor)
       .subscribe(resp => {
         console.log(resp);
+        this.peliculas = resp['results'];
+        this.buscando = false;
       });
 
   }
 
   buscarIdeas(idea: string) {
     this.textoBuscar = idea;
+  }
+
+
+  async detalle(id: string) {
+    const modal = await this.modalCtrl.create({
+      component: DetalleComponent,
+      componentProps: {
+        id
+      }
+    });
+
+    modal.present();
   }
 
 }
