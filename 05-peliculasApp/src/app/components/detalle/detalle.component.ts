704da5e1b01e57e5670f93c985905298f792989b
labdevjs <labdevjs@gmail.com>
2021-05-05 10:06:29 +0000

    139

--- 05-peliculasApp/src/app/components/detalle/detalle.component.ts
+++ 05-peliculasApp/src/app/components/detalle/detalle.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MoviesService } from 'src/app/services/movies.service';
-import { PeliculaDetalle } from 'src/app/interfaces/interfaces';
+import { PeliculaDetalle, Cast } from 'src/app/interfaces/interfaces';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detalle',
@@ -12,26 +13,40 @@ export class DetalleComponent implements OnInit {
   @Input() id: string;
 
   pelicula: PeliculaDetalle = {};
+  actores: Cast[] = [];
 
-  constructor( private moviesService: MoviesService) { }
+  oculto = 150;
+  slideOptActores = {
+    slidesPerView: 3.3,
+    freeMode: true,
+    spaceBetween: 0
+  };
+
+  constructor(private moviesService: MoviesService,
+    private modalCtrl: ModalController) { }
 
   ngOnInit() {
     console.log(this.id);
 
     this.moviesService.getPeliculaDetalle(this.id)
-      .subscribe( resp => {
+      .subscribe(resp => {
         console.log(resp);
 
         this.pelicula = resp;
 
       })
 
-      this.moviesService.getActoresPelicula(this.id)
-        .subscribe( resp => {
-          //console.log(resp);
+    this.moviesService.getActoresPelicula(this.id)
+      .subscribe(resp => {
+        //console.log(resp);
+        this.actores = resp.cast;
 
-        })
+      })
 
+  }
+
+  regresar() {
+    this.modalCtrl.dismiss();
   }
 
 }
